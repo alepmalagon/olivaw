8744b25c73c056b2d5d6cbd71bdb08534fa47422
Alejandro Perez Malagon <alepmalagon@gmail.com>
2016-12-28 11:22:30 -0500

    Added index htm for pages

---
+++ main.py
@@ -0,0 +1,11 @@
+main
+def foo():
+    doc = "The  property."
+    def fget(self):
+        return self._
+    def fset(self, value):
+        self._ = value
+    def fdel(self):
+        del self._
+    return locals()
+ = property(**())
